f0d9b507b4a44538b7578e67e81fa604a6f87666
David Blatcher <david.blatcher@graduate-jobs.com>
2020-03-30 21:36:55 +0100

    example emit event

--- src/js/socket.js
+++ src/js/socket.js
@@ -8,4 +8,14 @@ setTimeout(()=> {
     console.log ('connnected:', socket.connected)
 },2000)
 
+const testButton = document.querySelector('#test-button')
+
+function rollD6() {
+    let result = Math.floor((Math.random() * 6)+1)
+    console.log(result)
+    socket.emit('d6-roll', result)
+}
+
+testButton.addEventListener('click',rollD6)
+
 window.socket = socket
--- src/server/socket-handler.js
+++ src/server/socket-handler.js
@@ -1,6 +1,16 @@
 function socketHandler (io) {
     io.on('connection', function(socket){
         console.log('a user connected');
+        socket.on('disconnect', function(){
+            console.log('user disconnected');
+        });
+
+
+        socket.on('d6-roll', function(msg){
+            console.log('user rolled a d6: ' + msg);
+        });
+
+
     });
 }
 
