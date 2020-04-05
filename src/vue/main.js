83f0c70d1157595743634b238a0790fccd9c404f
David Blatcher <david.blatcher@graduate-jobs.com>
2020-04-05 13:56:18 +0100

    include sample vue app

---
+++ src/vue/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import App from './App.vue';
+
+let app = new Vue({
+  el: '#app',
+  render: h => h(App),
+});
+
+console.log(app)
+window.app = app
--- webpack.common.js
+++ webpack.common.js
@@ -1,34 +1,35 @@
 const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
     entry: {
         socket: './src/js/socket.js',
-        base: './src/scss/base.scss'
+        base: './src/scss/base.scss',
+        app: './src/vue/main.js',
     },
-
 
     module: {
         rules: [
-          {
-            test: /\.js$/i,
-            exclude: /node_modules/,
-          },
-          {
-            test: /\.s[ac]ss$/i,
-            exclude: /node_modules/,
-            use: [
-                {
-                    loader: 'file-loader',
-                    options: {
-                        name: 'stylesheets/[name].css'
-                    }
-                },
-                'sass-loader',
-            ],
-          },
+            { test: /\.vue$/, use: 'vue-loader' },
+            { test: /\.css$/, use: ['vue-style-loader', 'css-loader']},
+            { test: /\.js$/i, exclude: /node_modules/, },
+            { test: /\.s[ac]ss$/i, exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'stylesheets/[name].css'
+                        }
+                    },
+                    'sass-loader',
+                ],
+            },
         ],
     },
 
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
 
     output: {
         filename: `javascripts/[name].js`,
