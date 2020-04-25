317fe1e63dc48afca36fc53576e5efbe7c7776df
David Blatcher <david.blatcher@graduate-jobs.com>
2020-04-25 11:32:42 +0100

    PlayerCard component

---
+++ src/vue/components/PlayerCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <article v-bind:class="`${color} card`">
+      <h2>{{player.playerName}}</h2>
+  </article>
+</template>
+
+<script>
+export default {
+    props: ["player", "color","gm","local"],
+}
+</script>
+
+<style>
+
+</style>
--- src/vue/components/PlayersDisplay.vue
+++ src/vue/components/PlayersDisplay.vue
@@ -1,26 +1,28 @@
 <template>
   <section class="frame">
-      <div v-if="gameMaster" class="blue card">
-        <h2>GM:{{gameMaster.playerName}}</h2>
-        <RollZone v-bind="{rollData: lastDiceRoll, size:50}" ref="rollZone"/>
-      </div>
-      <div v-if="localPlayer" class="green card">
-        <h2>{{localPlayer.playerName}}</h2>
-        <input type="text"/>
-      </div>
-      <div class="red card" v-for="player in otherPlayers" v-bind:key="player.playerId">
-        <h2>{{player.playerName}}</h2>
 
-      </div>
+      <PlayerCard 
+      v-bind="{player: gameMaster, color:'blue', gm:true, local:(localPlayer===gameMaster)}" />
+
+      <PlayerCard 
+      v-if="localPlayer !== gameMaster"
+      v-bind="{player: localPlayer, color:'green', gm:false, local:true}" />
+
+      <PlayerCard 
+      v-for="player in otherPlayers" v-bind:key="player.playerId"
+      v-bind="{player: player, color:'red', gm:false, local:false}" />
+
   </section>
 </template>
 
 <script>
 
-import RollZone from "./RollZone.vue";
+
+import PlayerCard from "./PlayerCard.vue";
+
 
 export default {
-  components: {RollZone},
+  components: {PlayerCard},
   props: ["players", "playerId","gameMasterId", "lastDiceRoll"],
 
   computed: {
@@ -29,7 +31,6 @@ export default {
     },
 
     localPlayer: function() {
-      if (this.playerId === this.gameMasterId) return null
       return this.players.filter(player => player.playerId === this.playerId)[0]
     },
 
