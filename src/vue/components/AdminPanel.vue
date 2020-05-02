1145fe68ce17529d6b549d3cd0856d336eb9f7d7
David Blatcher <david.blatcher@graduate-jobs.com>
2020-05-02 13:38:27 +0100

    use JoinRequestPrompt for gm to answer join requests

--- src/vue/components/AdminPanel.vue
+++ src/vue/components/AdminPanel.vue
@@ -1,22 +1,45 @@
 <template>
-  <section class="frame">
-      <h3>Admin</h3>
+    <section>
+        <h3>Admin</h3>
 
-      <CloseGameButton  @close-game="requestGameClose"/>
+        <div class="frame">
+            <CloseGameButton  @close-game="requestGameClose"/>
 
-  </section>
+            <transition-group name="list" tag="span">
+                <join-request-prompt v-for="(request,index) in joinRequests" v-bind:key="index"
+                @answer="handleJoinRequestAnswer"
+                v-bind="{request: request}"/>
+            </transition-group>
+        </div>
+    </section>
 </template>
 
 <script>
 
 import CloseGameButton from './admin-panel/CloseGameButton.vue'
+import JoinRequestPrompt from './admin-panel/JoinRequestPrompt.vue'
 
 export default {
+    components: {CloseGameButton, JoinRequestPrompt},
     props : ['config', 'socket', 'playerId'],
 
-    components: {CloseGameButton},
+    data() {
+        return {
+            joinRequests: [],
+        }
+    },
 
     methods : {
+
+        handleJoinRequestAnswer (answer, request) {
+            const {joinRequests, socket, playerId, config} = this
+
+            if (joinRequests.indexOf(request) !== -1) {
+                joinRequests.splice( joinRequests.indexOf(request), 1)
+            }
+
+            socket.emit('entry-request-response',answer,request, playerId, config.gameId)
+        },
 
         requestGameClose() {
             const {playerId} = this
@@ -26,11 +49,9 @@ export default {
             this.socket.emit('gm-closing-game',{gameId, playerId})
         },
 
-        handleJoinRequest(data) {
-            console.log(data)
-            let answer = confirm(`Let ${data.player.playerName} into the game?`)
-            console.log(answer)
-            this.socket.emit('entry-request-response',answer,data, this.playerId, this.config.gameId)
+        handleJoinRequest(request) {
+            console.log(request)
+            this.joinRequests.push(request)
         },
     },
 
@@ -41,5 +62,17 @@ export default {
 </script>
 
 <style scoped>
+
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateX(-30px);
+}
 
 </style>
---
+++ src/vue/components/admin-panel/JoinRequestPrompt.vue
@@ -0,0 +1,33 @@
+<template>
+  <article>
+      <p>{{question}}</p>
+      <div class="button" @click="($event)=>{$emit('answer',true,request)}">yes</div>
+      <div class="button" @click="($event)=>{$emit('answer',false,request)}">no</div>
+  </article>
+</template>
+
+<script>
+export default {
+    props: ['request'],
+
+    computed: {
+        question() {
+            return `Let ${this.request.player.playerName} into the game?`
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+    article {
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+    }
+
+    p {
+        margin-left: auto;
+    }
+
+</style>
