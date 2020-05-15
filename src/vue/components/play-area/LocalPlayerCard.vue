be6e11a43339c63a7fe9fb3ffce1f8b837eeedab
David Blatcher <david.blatcher@graduate-jobs.com>
2020-05-15 22:44:55 +0100

    used folding panel on local player Card

--- src/vue/components/play-area/LocalPlayerCard.vue
+++ src/vue/components/play-area/LocalPlayerCard.vue
@@ -2,38 +2,44 @@
   <article v-bind:class="`${color} card`">
       <h2>{{player.playerName}}</h2>
 
-      <character-sheet-section v-for="(section, index) in groupedData" v-bind:key="'_'+index" v-bind="{section}">
-        <div class="display-cs-group__datum-wrapper" v-for="(datum, index2) in section.values" v-bind:key="index2">
+      <folding-panel v-bind="{title:'My Character Sheet', holderClass:'bordered', transitionClass:'corner-fold'}">
+        <button v-if="currentSheetItemName" @click="()=>{saveSheet(currentSheetItemName)}">save {{currentSheetItemName}}</button>
+        <button @click="openSaveSheetDialogue" >save as</button>
+        <button @click="openLoadSheetDialogue" >load</button>
 
-          <span class="display-cs-group__datum" 
-          v-if="datum.type ==='string'" >
-            <span class="display-cs-group__key">{{datum.name}}:</span> 
-            <input class="display-cs-group__value" @change="handleUpdate" type="text" v-model="datum.value"/>
-          </span>
+        <hr/>
 
-          <span class="display-cs-group__datum"
-          v-if="datum.type ==='number'">
-            <span class="display-cs-group__key">{{datum.name}}:</span> 
-            <span class="display-cs-group__value">
-              <input @change="handleUpdate" type="number" v-model="datum.value"/>
-              <span v-if="typeof datum.max !== 'undefined'">
-                &nbsp;/&nbsp;<input @change="handleUpdate" type="number" v-model="datum.max"/>
+        <character-sheet-section v-for="(section, index) in groupedData" v-bind:key="'_'+index" v-bind="{section}">
+          <div class="display-cs-group__datum-wrapper" v-for="(datum, index2) in section.values" v-bind:key="index2">
+
+            <span class="display-cs-group__datum" 
+            v-if="datum.type ==='string'" >
+              <span class="display-cs-group__key">{{datum.name}}:</span> 
+              <input class="display-cs-group__value" @change="handleUpdate" type="text" v-model="datum.value"/>
+            </span>
+
+            <span class="display-cs-group__datum"
+            v-if="datum.type ==='number'">
+              <span class="display-cs-group__key">{{datum.name}}:</span> 
+              <span class="display-cs-group__value">
+                <input @change="handleUpdate" type="number" v-model="datum.value"/>
+                <span v-if="typeof datum.max !== 'undefined'">
+                  &nbsp;/&nbsp;<input @change="handleUpdate" type="number" v-model="datum.max"/>
+                </span>
               </span>
             </span>
-          </span>
 
-          <list-control  v-if="datum.type ==='list'"
-          @change-quantity="(event)=>{handleListItemQuantityChange(event, datum)}" 
-          @change-item="(event)=>{handleListItemChange(event, datum)}" 
-          @delete-item="(event)=>{handleListItemDelete(event, datum)}"
-          @new-item="(event)=>{handleListItemAdd(datum)}"
-          v-bind="{datum}"/>
-        </div>
-      </character-sheet-section>
+            <list-control  v-if="datum.type ==='list'"
+            @change-quantity="(event)=>{handleListItemQuantityChange(event, datum)}" 
+            @change-item="(event)=>{handleListItemChange(event, datum)}" 
+            @delete-item="(event)=>{handleListItemDelete(event, datum)}"
+            @new-item="(event)=>{handleListItemAdd(datum)}"
+            v-bind="{datum}"/>
+          </div>
+        </character-sheet-section>
 
-      <button v-if="currentSheetItemName" @click="()=>{saveSheet(currentSheetItemName)}">save {{currentSheetItemName}}</button>
-      <button @click="openSaveSheetDialogue" >save as</button>
-      <button @click="openLoadSheetDialogue" >load</button>
+      </folding-panel>
+
       <roll-zone v-bind="{rollData, size:40}"/>
 
       <storage-dialogue v-bind="storageDialogueProps" 
@@ -51,11 +57,11 @@ import * as storage from "../../modules/storage";
 import StorageDialogue from '../StorageDialogue.vue'
 import ListControl from './ListControl.vue'
 import CharacterSheetSection from './CharacterSheetSection.vue'
+import FoldingPanel from '../FoldingPanel.vue'
 
-window.storage = storage
 
 export default {
-    components : {RollZone, StorageDialogue, ListControl, CharacterSheetSection},
+    components : {RollZone, StorageDialogue, ListControl, CharacterSheetSection, FoldingPanel},
     props: ["player", "color","gm", "rollData","characterSheet"],
     data() {
         return {
